38311b7c087c108d1db669a48f40d5ac38a928ae
root_who <joaopaulo_gonzaga@outlook.com>
2024-03-27 12:55:43 -0300

    chore: start project

---
+++ app/src/main/java/com/br/stone/job_interview/rickandmortytrivia/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.br.stone.job_interview.rickandmortytrivia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.br.stone.job_interview.rickandmortytrivia.ui.theme.RickAndMortyTriviaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyTriviaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickAndMortyTriviaTheme {
+        Greeting("Android")
+    }
+}
